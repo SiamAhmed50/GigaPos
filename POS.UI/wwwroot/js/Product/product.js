ebe6c6dc0deacb70a60fd92cdc01e53796d8ff7a
ARIFIN <shamsularifin.engr.cse@gmail.com>
2022-12-29 22:15:02 +0600

    Product

--- POS.UI/wwwroot/js/Product/product.js
+++ POS.UI/wwwroot/js/Product/product.js
@@ -17,40 +17,45 @@ function loadDataTable() {
                 "data": "imageUrl",
                 "render": function (data, type, full, meta) {
 
-                    return '<img src="'+data+'" width="100%" height="50px"/>'
+                    return '<img src="'+data+'" width="50%" height="30px"/>'
                 }
             },
-            { "data": "code", "width": "15%" },
-            { "data": "name", "width": "15%" },
-            { "data": "category.name", "width": "15%" },
+            { "data": "code", "width": "10%" },
+            { "data": "name", "width": "10%" },
+            { "data": "category.name", "width": "10%" },
             { "data": "brand.name", "width": "15%" },
           
-            { "data": "salePrice", "width": "15%" },
-            { "data": "purchaseCost", "width": "15%" },
+            { "data": "salePrice", "width": "10%" },
+            { "data": "purchaseCost", "width": "10%" },
             {
                 "data": "id",
                 "render": function (data) {
 
-                    return '<div class="w-75 btn-group" role = "group">'
-                        + '<a href="/Products/Upsert?Id=' + data + '" class="btn btn-primary mx-2" > <i class="bi bi-pencil-square"></i> Edit</a >' +
-                        '<a onClick="Delete(' + data + ')" class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a></div >' +
-                        '<img src="/Products/GenerateBarcode?productId='+data+'" width="100%" height="50px"/>';
+                    return '<div class="button-group">'
+                        + '<div class="getViewProduct">'
+                        + '  <buttontype="button" onClick="getAllById(' + data + ')"  class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#productsDetails"><i class="fa-solid fa-eye"></i></button> </div>'
+
+                        + '<div class="dropdown action-button mx-2">'
+                        + '<button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa-solid fa-gears"></i></button>'
+                        + '<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">'
+                        + '<li><a  href="/Products/Upsert?Id=' + data + '" class="dropdown-item btn btn-primary"><i class="bi bi-pencil-square"></i> Edit </a></li>'
+                        + '<li><a class="dropdown-item btn btn-primary" onClick="Delete(' + data + ')"><i class="bi bi-trash-fill"> </i> Delete</a> </li> </ul> </div>'
+
+                    +'<div class="barcode-box">'
+                        + '  <buttontype="button" onClick="getBarCode('+data+')"   class="btn btn-secondary btn-barCode" data-bs-toggle="modal" data-bs-target="#barCode"><i class="fa-solid fa-barcode"></i></button> </div>'
+                        
+
+                      
+                      
+
+
+                       
                 },
-                "width": "15%"
+                "width": "110%"
             },
         ]
 
-        //createdRow: function (row, data, index) {
-        //    debugger;
-        //    $.ajax({
-        //        url: "/Products/GenerateBarcode",
-        //        success: function (data) {
-        //            debugger;
-        //            $('td', row).eq(0).find("img").attr("src",data);
-
-        //        }
-        //    })
-        //},
+        
     });
 }
 
@@ -63,7 +68,7 @@ function loadDataTable() {
 
 
 function Delete(id) {
-    debugger;
+    
     Swal.fire({
         title: 'Are you sure?',
         text: "You won't be able to revert this!",
@@ -92,7 +97,88 @@ function Delete(id) {
 }
 
 
-//<div class="w-15 btn-group align-content-center" role="group">
-//    <a href="/Admin/Proucts/Upsert?id=${data}" class="btn btn-primary mx-1"><i class="bi bi-pencil-square"></i>&nbsp; Edit</a>
-//    <a class="btn btn-danger mx-1"><i class="bi bi-trash-fill"></i>&nbsp; Delete</a>
-//</div>
+function GetProductById(id) {
+   
+    var product = null;
+    $.ajax({
+
+        url: "/Products/GetById/" + id,
+        method: "GET",
+        type: "json",
+        async: false,
+
+        success: function (data) {
+           
+            product = data.data;
+           
+        }
+
+
+    });
+    
+    return product;
+}
+
+function getAllById(id) {
+
+    debugger
+    $("#tbl_productModal tbody tr").remove();  
+    $("#ProductmodalImage span img").remove();  
+    $("#staticBackdropLabel span ").remove();  
+    var product = GetProductById(id);
+   
+          
+         
+           
+            var imagediv = '<img src="' + product.imageUrl + '" class="product-modal-image"/>';
+            var title = '<span >' + product.name + ' </span>'
+            var rows = "<tr>"
+                + "<td>Code</td> "
+                + "<td class='prtoducttd'>" + product.code + "</td> </tr >"
+                + "<tr>"
+                + "<td>Category</td> "
+                + "<td class='prtoducttd'>" + product.category.name + "</td> </tr >"
+                + "<tr>"
+                + "<td>Brand</td> "
+                + "<td class='prtoducttd'>" + product.brand.name + "</td> </tr >"
+                + "<tr>"
+                + "<td> Price</td> "
+                + "<td class='prtoducttd'>" + product.salePrice + "</td> </tr >"
+                + "<tr>"
+                + "<td>Cost</td> "
+                + "<td class='prtoducttd'>" + product.purchaseCost + "</td> </tr >"
+                + "<td>Stock</td> "
+                + "<td class='prtoducttd'> </td> </tr >";
+               
+               
+            $('#tbl_productModal tbody').append(rows); 
+            $('#ProductmodalImage span ').append(imagediv); 
+            $('#staticBackdropLabel ').append(title); 
+        
+    
+    
+}
+
+function getBarCode(id) {
+   
+   
+    var item = GetProductById(id);
+   
+    $("#barCodeImage  img ").remove(); 
+
+    var Barcode =
+         '<img src="/Products/GenerateBarcode?productId=' + id + '" width="100%" height="50px"/>'
+
+    $('#barProductCode').text(item.code);
+    $('#barProductTitle').text(item.name);
+    $('#barProductPrice').text(item.salePrice+" Tk");
+
+    $('#barCodeImage  ').append(Barcode);
+
+}
+
+function barCodePrint() {
+    debugger
+    $("#printbarcode").printThis();
+   /* $("#printbarcode").print();*/
+}
