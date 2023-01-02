d373907eb36d1a4aadee5d40284ebca5a06f8d83
ARIFIN <shamsularifin.engr.cse@gmail.com>
2023-01-02 23:24:44 +0600

    Damagescrud

--- POS.UI/Controllers/DamagesController.cs
+++ POS.UI/Controllers/DamagesController.cs
@@ -1,17 +1,76 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using POS.DAL.Repository.IRpository;
+using POS.Models.AppVM;
+using POS.Models.EntityModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace POS.UI.Controllers
 {
     public class DamagesController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+      
+
+        public DamagesController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+          
+        }
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Upsert()
+        public IActionResult Upsert(int? id)
         {
+
+            DamageVM damageVM = new()
+            {
+                Damage = new()
+            };
+            if (id == null || id == 0)
+            {
+                ViewBag.ProductList=new SelectList(_unitOfWork.Product.GetAll(), "Id", "Name");
+				return View(damageVM);
+            }
+            else
+            {
+                damageVM.Damage = _unitOfWork.Damage.GetFirstOrDefault(x => x.Id == id);
+                return View(damageVM);
+            }
+
+
+
             return View();
+        }
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Upsert(DamageVM obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var model = new Damage();
+                model = obj.Damage;
+                //Create Product
+                if (obj.Damage.Id == 0)
+                {
+
+                    _unitOfWork.Damage.Add(model);
+                    _unitOfWork.Save();
+                    return RedirectToAction("Index");
+                }
+
+                //Update Products
+                else
+                {
+                    _unitOfWork.Damage.Update(model);
+                    _unitOfWork.Save();
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(obj);
         }
     }
 }
